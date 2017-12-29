649d0d84415ff6641343ef00426b274079172b27
Paul Colomiets <paul@colomiets.name>
2017-12-29 02:01:04 +0200

    Container versions are extracted

--- src/deploy/mod.rs
+++ src/deploy/mod.rs
@@ -1,5 +1,7 @@
-use std::process::Command;
-use std::collections::BTreeSet;
+use std::str::from_utf8;
+use std::ascii::AsciiExt;
+use std::process::{Command, Stdio};
+use std::collections::{BTreeSet, BTreeMap};
 
 pub mod config;
 pub mod spec;
@@ -9,10 +11,16 @@ pub use self::spec::{Spec, parse_spec_or_exit};
 
 use exit::ExitCode;
 
+
+fn check_ver(s: &str) -> bool {
+    s.len() > 0 &&
+        s.chars().all(|x| x.is_ascii() && x.is_alphanumeric() || x == '.')
+}
+
 pub fn main(config: Config, deployment: String) -> ! {
     let spec = parse_spec_or_exit(config);
     let mut exit = ExitCode::new();
-    let mut built = BTreeSet::new();
+    let mut built = BTreeMap::new();
     let mut failed = BTreeSet::new();
 
     let deployment = match spec.deployments.get(&deployment) {
@@ -25,12 +33,17 @@ pub fn main(config: Config, deployment: String) -> ! {
     let containers = deployment.commands.values().map(|x| &x.container)
         .chain(deployment.daemons.values().map(|x| &x.container));
     for container in containers {
-        let st = Command::new("vagga")
-            .arg("_capsule").arg("build").arg(container)
-            .status();
-        match st {
-            Ok(s) if s.success() => { }
-            Ok(s) => {
+        if built.contains_key(container) {
+            continue;
+        }
+        let output = Command::new("vagga")
+            .arg("_capsule").arg("build").arg(container).arg("--print-version")
+            .stderr(Stdio::inherit())
+            .stdout(Stdio::piped())
+            .output();
+        let ver_bytes = match output.as_ref().map(|x| (x.status, &x.stdout)) {
+            Ok((s, ver_bytes)) if s.success() => ver_bytes,
+            Ok((s, _)) => {
                 error!("Container {:?} failed to build with status: {}",
                     container, s);
                 exit.report_error();
@@ -43,13 +56,23 @@ pub fn main(config: Config, deployment: String) -> ! {
                 failed.insert(container.clone());
                 continue;
             }
-        }
-        built.insert(container.clone());
+        };
+        let version = match from_utf8(ver_bytes) {
+            Ok(s) if check_ver(&s.trim()) => s.trim().to_string(),
+            _ => {
+                error!("Invalid version returned for container {:?}: {:?}",
+                    container, String::from_utf8_lossy(&ver_bytes));
+                exit.report_error();
+                failed.insert(container.clone());
+                continue;
+            }
+        };
+        built.insert(container.clone(), version);
     }
 
     info!("Built containers {:?}", built);
     if !exit.is_ok() {
-        error!("Failed containers containers {:?}", failed);
+        error!("Failed containers {:?}", failed);
     }
     exit.exit_if_failed();
     eprintln!("Deploying {:?}", deployment);
