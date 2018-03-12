59c07420b36f9d5c8e3deed40f517e7871736c08
Paul Colomiets <paul@colomiets.name>
2018-03-13 00:24:11 +0200

    Smarter default version algorithm

--- src/version/git.rs
+++ src/version/git.rs
@@ -1,6 +1,7 @@
 use std::ffi::OsStr;
 
-use git2::{self, Repository, RepositoryOpenFlags, DescribeOptions};
+use git2::{self, Repository, RepositoryOpenFlags, DescribeOptions, Commit};
+use git2::{DescribeFormatOptions, StatusOptions, StatusShow};
 
 use exit::ExitCode;
 
@@ -10,17 +11,43 @@ pub fn describe(exit: &mut ExitCode) -> String {
         Ok(v) => v,
         Err(e) => {
             error!("git describe error: {}", e);
-            info!("HINT: Add a named tag to,current or any previous commit: \
-                git tag -a v0.1.0");
             exit.report_error();
             String::from("v0.0.0-unknown")
         }
     }
 }
 
+fn count_commits(c: &Commit) -> u64 {
+    let mut q = vec![c.clone()];
+    let mut cnt = 0;
+    while let Some(commit) = q.pop() {
+        cnt += 1;
+        for p in commit.parents() {
+            q.push(p);
+        }
+    }
+    cnt
+}
+
 fn get_version() -> Result<String, git2::Error> {
     let git_repo = Repository::open_ext("/work",
         RepositoryOpenFlags::empty(), &[] as &[&OsStr])?;
-    let describe = git_repo.describe(&DescribeOptions::default())?;
-    Ok(format!("{}", describe.format(None)?))
+    let mut opt = DescribeOptions::default();
+    opt.pattern("v[0-9]*");
+    let mut fopt = DescribeFormatOptions::new();
+    fopt.dirty_suffix("-dirty");
+    match git_repo.describe(&opt) {
+        Ok(ver) => return Ok(format!("{}", ver.format(Some(&fopt))?)),
+        Err(ref e) if e.message().find("no reference found").is_some() => {}
+        Err(e) => return Err(e),
+    }
+    // fallback in case no tags exists
+    let commit = git_repo.head()?.peel_to_commit()?;
+    let n = count_commits(&commit);
+    let mut sopt = StatusOptions::new();
+    sopt.show(StatusShow::IndexAndWorkdir);
+    let dirty = git_repo.statuses(Some(&mut sopt))?.iter().count() > 0;
+    Ok(String::from(format!("v0.0.0-{}-u{:.7}{}", n,
+        commit.id().to_string(),
+        if dirty { "-dirty" } else { "" })))
 }
